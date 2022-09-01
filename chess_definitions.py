e82b60ad2127c8e659099de8b5bb9531f6176610
orielcoding <89340298+orielcoding@users.noreply.github.com>
2022-09-01 17:34:39 +0300

    Add files via upload
    reorganizing the setup for validation process I

--- chess_definitions.py
+++ chess_definitions.py
@@ -1,48 +1,78 @@
 import numpy as np
 
-row = 8
-col = 8
 left_border = top_border = 0
 right_border = bottom_border = 7
 
 
+def create_board():
+    board = np.zeros([8, 8])
+    return board
+
+
+class Square:  # TODO FILL MEMBER OF THREATS AFTER DEFINING THE MOVEMENTS_TYPES IN NUMPY
+    def __init__(self, location: list, color=None, piece_type=None):
+        # default values None if the square isn't occupied.
+        self.type = piece_type
+        self.color = color
+        self.location = location
+
+    def get_type(self):
+        return self.type
+
+    def get_color(self):
+        return self.color
+
+    def set_type_and_color(self, new_type: str = None, new_color: int = None):
+        # default values None if the square isn't occupied.
+        # they both change every move for the requested square
+        self.type = new_type
+        self.color = new_color
+
+    def threats(self):
+        pass  # TODO FUNCTION GETS MOVEMENT TYPES AND RETURN LIST OF THREATENING SQUARES TO BE MAPPED?
+
+
+class Board:
+    def __init__(self, row_size: int, column_size: int):
+        self.board_matrix = np.zeros([row_size, column_size])
+        for row in self.board_matrix:
+            for column in row:
+                self.board_matrix[row, column] = None  # TODO FILL IN THE RELEVANT PARAMETERS
+
+    def get_square_state(self, location: list):  # returns what's located in specific square
+        return self.board_matrix[location]  # supposed to return an instance of Piece object, or None
+
+    def set_square_state(self, location: list, piece: object):
+        # piece object should contain the type and color of the piece
+        # location contains the user's input: current square and target square
+        self.board_matrix[location[0]] = None  # the current square
+        self.board_matrix[location[1]] = Square(location[1]).set_type_and_color(piece.type,piece.color)  # the target square
+
+
 class Piece:
-    def __init__(self, starting_location: list, color: int, piece_movement):  # colors: 1=white , 2=black
+    def __init__(self, piece_type: str, starting_location: list, color: int):  # colors: 0=white , 1=black
         self.location = starting_location
         self.color = color
-
-    # self.__movement = piece_movement  # should be a list of 2 integers, which possibly vary according to input
-
-    # לברר מה ההבדל בלקרוא למתודה דרך self או דרך הקלאס
+        self.type = piece_type
 
     def get_location(self):
         return self.location
 
-    def set_location(self, piece_movement) -> (str):  # each possible move define how much to step at the row and col.
-        # must add the condition of a piece blockading the route to specific cube, which makes it illegal.
-        if piece_movement == "black_pawn_moves":
-            if self.get_location()[0] == 1:
-                possible_moves = [[2, 0], [1, 0]]
-            else:
-                possible_moves = [[1, 0]]
-        elif piece_movement == "white_pawn_moves":
-            if self.get_location()[0] == 6:
-                possible_moves = [[-2, 0], [-1, 0]]
-            else:
-                possible_moves = [[-1, 0]]
-        elif piece_movement == "bishop_moves":
-            possible_moves = self.bishop_moves()
-        elif piece_movement == "rook_moves":
-            possible_moves = self.rook_moves()
-        elif piece_movement == "knight_jumps":
-            possible_moves = self.knight_jumps
-        elif piece_movement == "queen_moves":
-            possible_moves = self.queen_moves()
-        elif piece_movement == "king_moves":
-            possible_moves = self.king_moves()
+    def white_pawn(self): # todo add case of eating
+        if self.get_location()[0] == 6:
+            possible_moves = [[-2, 0], [-1, 0]]
+        else:
+            possible_moves = [[-1, 0]]
         return possible_moves
 
-    def rook_moves(self):
+    def black_pawn(self):  # todo add case of eating
+        if self.get_location()[0] == 1:
+            possible_moves = [[2, 0], [1, 0]]
+        else:
+            possible_moves = [[1, 0]]
+        return possible_moves
+
+    def rook(self):
         possible_moves = []
         loc = self.get_location()
         while loc[0] < 7:
@@ -62,7 +92,7 @@ class Piece:
             possible_moves.append([loc[0], loc[1]])
         return possible_moves
 
-    def bishop_moves(self):
+    def bishop(self):
         possible_moves = []
         loc = self.get_location()
         while loc[0] < top_border and loc[1] < right_border:
@@ -86,7 +116,7 @@ class Piece:
             possible_moves.append([loc[0], loc[1]])
         return possible_moves
 
-    def knight_jumps(self):
+    def knight(self):
         possible_moves = []
         loc = self.get_location()
         while loc[0] < top_border - 1 and loc[1] < right_border:
@@ -130,16 +160,40 @@ class Piece:
             possible_moves.append([loc[0], loc[1]])
         return possible_moves
 
-    def queen_moves(self):
-        straight_line_moves = self.rook_moves()
+    def queen(self):
+        straight_line_moves = self.rook()
         diagnol_moves = self.bishop_movement()
         return (straight_line_moves + diagnol_moves)
 
-    def king_moves(self):  # need to fill #special case of stepping into check.
-        pass
+    def king(self):  # need to fill #special case of stepping into check.
+        loc = self.get_location()
+        possible_moves = [[loc[0] + 1, loc[1]], [loc[0] - 1, loc[1]], [loc[0], loc[1] + 1], [loc[0], loc[1] - 1],
+                          [loc[0] + 1, loc[1] + 1], [loc[0] + 1, loc[1] - 1], [loc[0] - 1, loc[1] + 1],
+                          [loc[0] - 1, loc[1] - 1]]
+        return possible_moves
+
+    def available_squares(self, piece_movement: str):
+        if piece_movement == "black_pawn_moves":
+            possible_moves = self.black_pawn()
+        elif piece_movement == "white_pawn_moves":
+            possible_moves = self.white_pawn()
+        elif piece_movement == "bishop_moves":
+            possible_moves = self.bishop()
+        elif piece_movement == "rook_moves":
+            possible_moves = self.rook()
+        elif piece_movement == "knight_jumps":
+            possible_moves = self.knight
+        elif piece_movement == "queen_moves":
+            possible_moves = self.queen()
+        elif piece_movement == "king_moves":
+            possible_moves = self.king()
+        return possible_moves
+
+    # def set_piece_location(self, piece_movement: str, current_square: list): # this function will be called only after validation
+    #     Board.set_square_state() = [self.type, self.color]
 
 
-class Moves:
+class MovementTypes:
     def __int__(self, loc: list):
         self.row = loc[0]
         self.column = loc[1]
@@ -152,10 +206,10 @@ class Moves:
 
     def diagonal(self):  # must test it
         axis = self.find_diagonal()
-        diag = np.diagonal(board, axis=axis)
+        diag = np.diagonal(create_board(), axis=axis)
 
     def flipped_diagonal(self):  # must test it
-        flipped_board = np.fliplr(board)
+        flipped_board = np.fliplr(create_board())
         distance = abs(self.row - 4)
         if self.row < 4:
             self.row = self.row + distance
@@ -164,47 +218,7 @@ class Moves:
 
         axis = self.find_diagonal()
         diagonal_indices = []
-        for index, x in np.ndenumerate(board, axis=axis):
-            # diag = np.diagonal(board, axis=axis)
-            diagonal_indices.append(index)
-        return diagonal_indices
-
-
-if __name__ == "__main__":  # preffered: change locations on the board not only in the piece location
-    #####NEED TO DEFINE PAWNS#####
-    board = np.zeros((row, col))
-    WB1 = Piece([-1, 2], 1, 'bishop_moves')
-    BB1 = Piece([0, 2], 2, 'bishop_moves')
-    WB2 = Piece([-1, 5], 1, 'bishop_moves')
-    BB2 = Piece([0, 5], 2, 'bishop_moves')
-    WR1 = Piece([-1, ], 1, 'rook_moves')
-    BR1 = Piece([0, 0], 2, 'rook_moves')
-    WR2 = Piece([-1, 7], 1, 'rook_moves')
-    BR2 = Piece([0, 7], 2, 'rook_moves')
-    WK1 = Piece([-1, 1], 1, 'knight_jumps')
-    BK1 = Piece([0, 1], 2, 'knight_jumps')
-    WK2 = Piece([-1, 6], 1, 'knight_jumps')
-    BK2 = Piece([0, 6], 2, 'knight_jumps')
-    WQ = Piece([-1, 4], 1, 'queen_moves')
-    BQ = Piece([0, 4], 2, 'queen_moves')
-    WKING = Piece([-1, 5], 1, 'king_moves')
-    BKING = Piece([0, 5], 2, 'king_moves')
-    WP1 = Piece([6,0],1,'white_pawn_moves')
-    WP2 = Piece([6, 1], 1, 'white_pawn_moves')
-    WP3 = Piece([6, 2], 1, 'white_pawn_moves')
-    WP4 = Piece([6, 3], 1, 'white_pawn_moves')
-    WP5 = Piece([6, 4], 1, 'white_pawn_moves')
-    WP6 = Piece([6, 5], 1, 'white_pawn_moves')
-    WP7 = Piece([6, 6], 1, 'white_pawn_moves')
-    WP8 = Piece([6, 7], 1, 'white_pawn_moves')
-    BP1 = Piece([1, 0], 2, 'black_pawn_moves')
-    BP2 = Piece([1, 1], 2, 'black_pawn_moves')
-    BP3 = Piece([1, 2], 2, 'black_pawn_moves')
-    BP4 = Piece([1, 3], 2, 'black_pawn_moves')
-    BP5 = Piece([1, 4], 2, 'black_pawn_moves')
-    BP6 = Piece([1, 5], 2, 'black_pawn_moves')
-    BP7 = Piece([1, 6], 2, 'black_pawn_moves')
-    BP8 = Piece([1, 7], 2, 'black_pawn_moves')
-
-
-
+        # for index, x in np.ndenumerate(board, axis=axis):
+        #     # diag = np.diagonal(board, axis=axis)
+        #     diagonal_indices.append(index)
+        # return diagonal_indices
