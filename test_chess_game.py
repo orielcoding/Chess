91591936dab1708793a74cdce17d3ab52f6e3943
orielcoding <89340298+orielcoding@users.noreply.github.com>
2022-09-16 23:34:59 +0300

    Add files via upload
    add tests to methods

--- test_chess_game.py
+++ test_chess_game.py
@@ -4,6 +4,7 @@ import pytest
 import numpy as np
 from numpy import testing
 
+
 def test_move_is_in_board():
     game = chess_game.Game()
     input_1 = game.move_is_in_board("a2 a3")
@@ -28,6 +29,8 @@ def test_color_move_validation():
     try_2 = game.color_move_validation(((6, 0), (3, 3)))
     assert try_2 == None
 
+#the following tests will accept only legal squares on the board.
+
 
 def test_coordinates():
     vector_1 = chess_definitions.Coordinates(((6, 0), (5, 0)))
@@ -40,12 +43,43 @@ def test_coordinates():
     testing.assert_array_equal(vector_3.vector, np.array([0, 0]))
     testing.assert_array_equal(vector_3.sign_vector, np.array([0, 0]))
 
-def test_squares_path():
-    c = chess_definitions.Coordinates(((7, 1), (4, 1)))
-    dist_1 = c.squares_path()
-    testing.assert_array_equal(dist_1 , np.array([[6,1],[5,1]]))
 
-# def test_movement_type_validation():
-#     game = chess_game.Game()
-#     with pytest.raises(ValueError) as e_info:
-#         game.movement_type_validation(((6, 0), (6, 1)),chess_definitions.Pawn(0))
+def test_squares_path():  # doesn't test real moves.
+    try_1 = chess_definitions.Coordinates(((7, 1), (4, 1)))
+    dist_1 = try_1.squares_path()
+    testing.assert_array_equal(dist_1 , np.array([[6,1],[5,1],[4,1]]))
+
+    try_2 = chess_definitions.Coordinates(((6, 1), (7, 5)))
+    dist_2 = try_2.squares_path()
+    testing.assert_array_equal(dist_2, np.array([[7,2], [8,3], [9, 4],[10,5]]))
+
+
+def test_movement_type_validation():
+    game = chess_game.Game()
+    with pytest.raises(ValueError) as e_info:
+        game.movement_type_validation(((6, 0), (6, 1)),chess_definitions.Pawn(0))  # pawn needs private case of eating
+    with pytest.raises(ValueError) as e_info:
+        game.movement_type_validation(((5, 0), (6,1)),chess_definitions.Knight(0))
+    with pytest.raises(ValueError) as e_info:
+        game.movement_type_validation(((2, 0), (5, 0)),chess_definitions.Bishop(0))
+    with pytest.raises(ValueError) as e_info:
+        game.movement_type_validation(((6, 0), (3, 3)),chess_definitions.Rook(0))
+
+
+def test_get_square_state():
+    game=chess_game.Game()
+    assert type(game.gameBoard.get_square_state((0,0))) == chess_definitions.Rook
+    assert type(game.gameBoard.get_square_state((7,0))) == chess_definitions.Rook
+    assert type(game.gameBoard.get_square_state((7,3))) == chess_definitions.Queen
+    assert type(game.gameBoard.get_square_state((7,1))) == chess_definitions.Knight
+    assert type(game.gameBoard.get_square_state((0,5))) == chess_definitions.Bishop
+    assert type(game.gameBoard.get_square_state((1,2))) == chess_definitions.Pawn
+
+
+def test_path_interruptions_validation():
+    game = chess_game.Game()
+    assert game.path_interruptions_validation(((5, 0), (6,1)),chess_definitions.Knight(0)) is None
+    assert game.path_interruptions_validation(((2, 1), (4,3)), chess_definitions.Bishop(0)) is None
+    with pytest.raises(ValueError) as e_info:
+        game.path_interruptions_validation(((0, 1), (4,5)),chess_definitions.Bishop(0))
+
